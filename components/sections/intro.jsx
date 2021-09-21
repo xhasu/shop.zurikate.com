a00a73a91e2fbdc3f3390013ec4857e18780b3eb
xhasu <ing.xhasu@gmail.com>
2021-09-20 20:36:29 -0500

    update colors bars, update poster

--- components/sections/intro.jsx
+++ components/sections/intro.jsx
@@ -53,8 +53,8 @@ const Intro = () => {
 					<img src="/media/video-poster.jpg" alt="Zurikate video poster" width="1920" height="1080" />
 				</video>
 				<div className="intro-play-button" onClick={() => setOpen(true)}>
-					<svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth=".2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth=".2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-					<span>Ver video</span>
+					<svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+					<span>Play video</span>
 				</div>
 				<div className="intro-scroll" onClick={() => handleScroll("#showcase", 80)}>
 					<span>Scroll Down</span>
