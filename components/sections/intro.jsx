fe11f4b0002501b767fcf4a2aa85c266f182be4e
xhasu <ing.xhasu@gmail.com>
2021-10-01 20:29:27 -0500

    update responsive video

--- components/sections/intro.jsx
+++ components/sections/intro.jsx
@@ -48,10 +48,16 @@ const Intro = () => {
 	return (
 		<div className="intro" id="intro">
 			<div className="intro-background">
-				<video ref={ref} preload="metadata" autoPlay={true} loop="loop" muted={true} playsInline="playsinline" poster="/media/video-poster.jpg">
+
+				<video className="intro-video-desktop" ref={ref} preload="metadata" autoPlay={true} loop="loop" muted={true} playsInline="playsinline" poster="/media/video-poster.jpg">
 					<source src="/media/zurikate-intro.mp4" type="video/mp4" />
 					<img src="/media/video-poster.jpg" alt="Zurikate video poster" width="1920" height="1080" />
 				</video>
+				<video className="intro-video-mobile" ref={ref} preload="metadata" autoPlay={true} loop="loop" muted={true} playsInline="playsinline" poster="/media/video-poster.jpg">
+					<source src="/media/zurikate-intro-mobile.mp4" type="video/mp4" />
+					<img src="/media/video-poster.jpg" alt="Zurikate video poster" width="1920" height="1080" />
+				</video>
+
 				<div className="intro-play-button" onClick={() => setOpen(true)}>
 					<svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
 					<span>Play video</span>
