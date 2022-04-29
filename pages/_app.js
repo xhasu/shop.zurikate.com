c158950ba0a10fa6aac1c1ad026073194431d4a5
xhasu <ing.xhasu@gmail.com>
2022-04-29 14:24:49 -0500

    update ui and deps

--- pages/_app.js
+++ pages/_app.js
@@ -13,9 +13,6 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
-        <link rel="preconnect" href="https://cdnjs.cloudflare.com" />
-	      <link rel="dns-prefetch" href="https://cdnjs.cloudflare.com" />
-        <link rel="stylesheet" href="/css/fonts.css"/>
       </Head>
       <Main>
         <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,9 @@ class MyDocument extends Document {
           <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-M3X677F');`}} />
           <script src="https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js"></script>
 	        <link rel="icon" href="favicon.ico?v=zurikate#1.0.0" />
+          <link rel="preconnect" href="https://cdnjs.cloudflare.com" />
+	        <link rel="dns-prefetch" href="https://cdnjs.cloudflare.com" />
+          <link rel="stylesheet" href="/css/fonts.css"/>
         </Head>
         <body>
           <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M3X677F" height="0" width="0" style="display:none;visibility:hidden"></iframe>`}} />
