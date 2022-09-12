44489e34d3f2ac94aa2d79b0ceabc78c75a59057
xhasu <ing.xhasu@gmail.com>
2022-09-11 21:47:19 -0500

    fix skins, add conversion tracking tag

--- components/shop/skins.jsx
+++ components/shop/skins.jsx
@@ -1,20 +1,14 @@
-import React from 'react'
-import SwiperCore, { Navigation } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React from "react";
+import SwiperCore, { Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 SwiperCore.use([Navigation]);
 
 const Skin = ({ data = {} }) => {
+  const { title = "", images = [], description = "", descriptionHtml = "" } = data;
 
-  const {
-    title = "",
-    images = [],
-    description = "",
-    descriptionHtml = ""
-  } = data;
-
-  const skinImage = images.find(item => {
-     return item.altText ? item.altText.toLowerCase().includes("skin") : {};
+  const skinImage = images.find((item) => {
+    return item.altText ? item.altText.toLowerCase().includes("skin") : {};
   });
 
   return (
@@ -25,45 +19,27 @@ const Skin = ({ data = {} }) => {
       <div className="skin-title">
         <strong>{title}</strong>
       </div>
-      <div className="skin-description" dangerouslySetInnerHTML={{__html: descriptionHtml}}></div>
+      <div className="skin-description" dangerouslySetInnerHTML={{ __html: descriptionHtml }}></div>
     </div>
-  )
-}
+  );
+};
 
 const Skins = ({ data = [], brand = "", setSkin }) => {
-
-  const skins = data.find(d => d.title == brand);
+  const skins = data.find((d) => d.title == brand);
 
   return (
     <div className="skins" id="skins">
       <div className="skins-container">
         <section className="section skins-section">
-
-          <div className="skins-swiper">
-            <Swiper navigation={{ prevEl: '.skins .swiper-arrows .arrow-prev', nextEl: '.skins .swiper-arrows .arrow-next' }} slidesPerView="auto" spaceBetween={20} centeredSlides={skins.products <= 1}>
-              {skins.products.map((skin, index) => {
-                return (
-                  <SwiperSlide key={index} onClick={() => setSkin(skin)}>
-                    <Skin data={skin} />
-                  </SwiperSlide>
-                )
-              })}
-            </Swiper>
+          <div className="skins-grid">
+            {skins.products.map((skin, index) => {
+              return <Skin key={index} data={skin} />;
+            })}
           </div>
-
-          <div className="swiper-arrows">
-            <div className="arrow-prev">
-              <img src="/images/icons/icon-angle-left.png" alt="icon arrow prev" loading="lazy" width="17" height="25" />
-            </div>
-            <div className="arrow-next">
-              <img src="/images/icons/icon-angle-right.png" alt="icon arrow next" loading="lazy" width="17" height="25" />
-            </div>
-          </div>
-
         </section>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skins
+export default Skins;
--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,8 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-M3X677F');`}} />
+          <script async={true} src="https://www.googletagmanager.com/gtag/js?id=AW-10964336005"></script>
+          <script dangerouslySetInnerHTML={{__html: `gtag('config', 'AW-10964336005');`}}></script>
           <script src="https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js"></script>
 	        <link rel="icon" href="favicon.ico?v=zurikate#1.0.0" />
           <link rel="preconnect" href="https://cdnjs.cloudflare.com" />
@@ -19,7 +21,7 @@ class MyDocument extends Document {
           <link rel="stylesheet" href="/css/fonts.css"/>
         </Head>
         <body>
-          <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M3X677F" height="0" width="0" style="display:none;visibility:hidden"></iframe>`}} />
+          <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.goo\admingletagmanager.com/ns.html?id=GTM-M3X677F" height="0" width="0" style="display:none;visibility:hidden"></iframe>`}} />
           <Main />
           <NextScript />
         </body>
