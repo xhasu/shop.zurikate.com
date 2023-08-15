537ab18059a1f99eddeac68b403c98c535c26a86
xhasu <ing.xhasu@gmail.com>
2023-08-14 20:36:04 -0500

    add icon select

--- components/ui/select.jsx
+++ components/ui/select.jsx
@@ -1,61 +1,65 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from "react";
+import { ChevronDownIcon, ChevronUpIcon } from "../shared/icons";
 
 const Select = (props) => {
-
-  const {
-    options = [],
-    placeholder,
-    keyValue,
-    keyShow,
-    handleClick,
-  } = props;
+  const { options = [], placeholder, keyValue, keyShow, handleClick } = props;
 
   const [open, setOpen] = useState(false);
   const ref = useRef();
 
   useEffect(() => {
-
-    if( open ) {
+    if (open) {
       // ref.current.scrollIntoView({'behavior': 'smooth', 'block': 'center'});
     }
-  
-    return () => { }
-  }, [open])
+
+    return () => {};
+  }, [open]);
 
   useEffect(() => {
-
     const mouseLeadeHandler = (e) => {
       setOpen(false);
     };
 
-    ref.current && ref.current.addEventListener('mouseleave', mouseLeadeHandler);
-    
+    ref.current && ref.current.addEventListener("mouseleave", mouseLeadeHandler);
+
     return () => {
-      ref.current && ref.current.removeEventListener('mouseleave', mouseLeadeHandler);
-    }
-  }, [])
+      ref.current && ref.current.removeEventListener("mouseleave", mouseLeadeHandler);
+    };
+  }, []);
 
   const handleClickItem = (e) => {
     handleClick(e.currentTarget.innerHTML);
     setOpen(false);
-  }
-
+  };
   if (options.length === 0) return null;
 
   return (
-    <div className={`ui-select ${open ? 'ui-select-open': ''}`} ref={ref}>
-      { placeholder && <div className="ui-select-placeholder" onClick={() => {setOpen(prev => !prev)}}>{placeholder}</div>}
+    <div className={`relative ui-select ${open ? "ui-select-open" : ""}`} ref={ref}>
+      {placeholder && (
+        <div
+          className="ui-select-placeholder"
+          onClick={() => {
+            setOpen((prev) => !prev);
+          }}>
+          {placeholder}
+        </div>
+      )}
       <div className="ui-select-list" hidden={!open}>
-        { options.filter(item => item.title != placeholder).map((item, index) => {
-          return (
-            <div className="ui-select-item" onClick={handleClickItem} key={index} value={item[keyValue]}>
-              {item[keyShow]}
-            </div>
-          )
-        }) }
+        {options
+          .filter((item) => item.title != placeholder)
+          .map((item, index) => {
+            return (
+              <div className="ui-select-item" onClick={handleClickItem} key={index} value={item[keyValue]}>
+                {item[keyShow]}
+              </div>
+            );
+          })}
+      </div>
+      <div className="absolute top-1/2 -translate-y-1/2 right-5 select-none cursor-pointer">
+        {open ? <ChevronUpIcon /> : <ChevronDownIcon />}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Select
+export default Select;
