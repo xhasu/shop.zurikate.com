dafb39b07d1ed7847c5461c1bc09584ec31252c6
xhasu <ing.xhasu@gmail.com>
2024-06-06 22:04:45 -0500

    update swiper time

--- components/sections/showcase.jsx
+++ components/sections/showcase.jsx
@@ -82,10 +82,10 @@ const Showcase = () => {
             pagination={{ clickable: true }}
             effect={"creative"}
             loop={true}
-            // autoplay={{
-            //   delay: 15000,
-            //   disableOnInteraction: false,
-            // }}
+            autoplay={{
+              delay: 2000,
+              disableOnInteraction: false,
+            }}
             creativeEffect={creativeEffect}>
             <SwiperSlide className="swiper-slide mb-6 md:mb-12 xl:mb-20">
               <div className="showcase-item relative">
